18b335d1d5e79b41aecc8f543c537401d1539c98
Tommy Figa <tomasfvfx@outlook.com>
2025-09-24 18:15:24 +0100

    fix: update SimpleGet to return *http.Response and handle response correctly

--- internal/httpclient/client.go
+++ internal/httpclient/client.go
@@ -1,24 +1,39 @@
 package httpclient
 
 import (
-	"io"
 	"net/http"
+	"strings"
+	"time"
 )
 
-
-func SimpleGet(url string) (string, error) {
+func SimpleGet(url string) (*http.Response, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	defer resp.Body.Close()
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(bodyBytes), nil
+	return resp, nil
 }
 
+func SendRequest(url string, method string, headers map[string]string, body string) (*http.Response, error) {
+	if method == "" {
+		return SimpleGet(url)
+	}
+
+	req, err := http.NewRequest(method, url, strings.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	for key, val := range headers {
+		req.Header.Set(key, val)
+	}
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
