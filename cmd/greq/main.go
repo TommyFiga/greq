2c4aaca91852886c282c08230e51934a6b896a24
Tommy Figa <tomasfvfx@outlook.com>
2025-09-25 23:19:39 +0100

    refactor: streamline error handling and response formatting in main function

--- cmd/greq/main.go
+++ cmd/greq/main.go
@@ -5,25 +5,35 @@ import (
 	"os"
 
 	"github.com/TommyFiga/greq/internal/httpclient"
+	"github.com/TommyFiga/greq/internal/output"
 	"github.com/TommyFiga/greq/internal/parser"
 	"github.com/TommyFiga/greq/internal/printer"
 )
 
-
 func main() {
-	opts,err := parser.ParseArgs()
+	opts, err := parser.ParseArgs()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
-	
-	resp, err := httpclient.SendRequest(opts.URL, opts.Method, opts.Headers, opts.Body)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
+		errorExit(fmt.Errorf("parser: %w", err))
 	}
 
-	res := printer.FormatResponse(resp, opts.IncludeHeaders, opts.PrettyJSON)
+	respData, err := httpclient.SendRequest(opts.URL, opts.Method, opts.Headers, opts.Body)
+	if err != nil {
+		errorExit(fmt.Errorf("client: %w", err))
+	}
 
-	fmt.Println(res)
+	fmtResp := printer.FormatResponse(respData, opts.IncludeHeaders, opts.PrettyJSON)
+
+	if len(opts.OutputFile) > 0 {
+		err := output.WriteResponseContentToFile(fmtResp, opts.OutputFile)
+		if err != nil {
+			errorExit(fmt.Errorf("output file: %w", err))
+		}
+	} else {
+		fmt.Println(fmtResp)
+	}
+}
+
+func errorExit(err error) {
+	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	os.Exit(1)
 }
